9dfbd4754ebdd2b53062c453a902dc472114f61c
Rhys Short <rhyshort@uk.ibm.com>
2016-02-01 14:49:12 +0000

    Enable plug-in to be built with travis
    - Add travis.yml
      - Only contains androud support
      - it needs to use arm emulator since x86 is not supported on travis.
    - Add set up script to replicate anmialdb from cloudant
    - Increase jasmine timeout so tests have a large oppertunity to pass.

---
+++ setup.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+system("curl -X POST http://127.0.0.1:5984/_replicate -H 'Content-Type: application/json' -d '{\"source\": \"http://clientlibs-test.cloudant.com/animaldb\",\"target\": \"http://localhost:5984/animaldb\",\"create_target\": true,\"continuous\": false}'")
+
+exit $?.to_i
