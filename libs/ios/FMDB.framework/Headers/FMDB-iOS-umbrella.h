c224eff47a18520872cff6835fa5521c619be63d
Bryn Harding <brynhard@uk.ibm.com>
2016-12-08 09:13:14 +0000

    Update CDTDatastore with framework built from CDTDatastore 1.2.0

--- libs/ios/FMDB.framework/Headers/FMDB-iOS-umbrella.h
+++ libs/ios/FMDB.framework/Headers/FMDB-iOS-umbrella.h
@@ -1,11 +1,7 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#endif
 
-#import "FMDatabase.h"
-#import "FMDatabaseAdditions.h"
-#import "FMDatabasePool.h"
-#import "FMDatabaseQueue.h"
-#import "FMDB.h"
-#import "FMResultSet.h"
 #import "FMDatabase.h"
 #import "FMDatabaseAdditions.h"
 #import "FMDatabasePool.h"
