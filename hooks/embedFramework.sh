16f3a73bee52a9bba3f54273ee8334212560d685
Bryn Harding <brynhard@uk.ibm.com>
2017-03-14 18:09:19 +0000

    Use the correct PRODUCT_NAME in LD_RUNPATH_SEARCH_PATHS
    Update README.md to describe process for running on iOS device.

--- hooks/embedFramework.sh
+++ hooks/embedFramework.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i .orig 's/\(LD_RUNPATH_SEARCH_PATHS = "[^"]*\)\(".*$\)/\1 \${PROJECT_DIR}\/HelloCordova\/Plugins\/cloudant-sync\2/' platforms/ios/HelloCordova.xcodeproj/project.pbxproj
+sed -i .orig 's/\(LD_RUNPATH_SEARCH_PATHS = "[^"]*\)\(".*$\)/\1 \${PROJECT_DIR}\/${PRODUCT_NAME}\/Plugins\/cloudant-sync @executable_path\/SharedFrameworks\2/' platforms/ios/*.xcodeproj/project.pbxproj
